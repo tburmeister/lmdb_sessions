ff62c93da06cdb89ad6382ddf2bc803048efc92c
tburmeister <burmeister.taylor@gmail.com>
2017-04-23 14:42:09 -0400

    some cleaning

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(
     name='lmdb_sessions',
@@ -8,6 +8,7 @@ setup(
     description='An LMDB based backend for CherryPy sessions',
     url='https://github.com/tburmeister/lmdb_sessions',
     author='Taylor Burmeister',
+    author_email='burmeister.taylor@gmail.com',
     install_requires=['cherrypy >= 5.0.1', 'lmdb >= 0.92'],
     packages=['lmdb_sessions']
 )
